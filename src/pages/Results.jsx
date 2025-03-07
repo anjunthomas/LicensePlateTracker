1b3b32c1d79bd68955f94456759329ccc09e030e
anjunthomas <anjunoblethomas@gmail.com>
2025-03-06 20:22:52 -0500

    Created license plate component. Upon search, license plate displays for the corresponding owner name, make, or model in real time.

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,7 +1,8 @@
-import "../styles.css";
+import "../styles/Results.css";
 import PlateDetails from "../components/PlateDetails";
 import SearchBar from "../components/SearchBar";
 import {useState } from "react";
+import PlateImage from "../components/PlateImage";
 
 // dummy data in JSON format to test search functionality
 const fakeDatabase = [
@@ -11,6 +12,8 @@ const fakeDatabase = [
         model: '718 Cayman G14',
         make: 'Porsche',
         location: 'Chicago, Illinois',
+        licensePlate: "1802600",
+        imageUrl: "/images/licensePlate1IL.jpg", // if images are in the public folder, they're already accessible so all you need is the rest of the path
         id: 1
     }, 
     {
@@ -19,6 +22,8 @@ const fakeDatabase = [
         model: 'Corvette Z06',
         make: 'Chevrolet',
         location: 'Seattle, Washington',
+        licensePlate: "AZM9590",
+        imageUrl: "/images/licensePlate2WA.jpg",
         id: 2
     }, 
 
@@ -28,6 +33,8 @@ const fakeDatabase = [
         model: 'GT-R Nismo',
         make: 'Nissan',
         location: 'Brooklyn, New York',
+        licensePlate: "ABC 1234",
+        imageUrl: "/images/licensePlate3NY.jpg",
         id: 3
     }, 
 
@@ -37,6 +44,8 @@ const fakeDatabase = [
         model: 'Camaro ZL1',
         make: 'Chevrolet',
         location: 'Honolulu, Hawaii',
+        licensePlate: "ALOHA",
+        imageUrl: "/images/licensePlate4HI.jpg",
         id: 4
     }, 
     {
@@ -45,6 +54,8 @@ const fakeDatabase = [
         model: 'F-Type R 75 Coupe',
         make: 'Jaguar',
         location: 'Los Angeles, California',
+        licensePlate: "8TRS777",
+        imageUrl: "/images/licensePlate5CA.jpg", 
         id: 5
     }, 
     {
@@ -53,7 +64,29 @@ const fakeDatabase = [
         model: 'GR Supra 3.0',
         make: 'Toyota ',
         location: 'Orlando, Florida',
+        licensePlate: "ANY TEXT",
+        imageUrl: "/images/licensePlate6FL.jpg",
         id: 6
+    }, 
+    {
+        owner: 'Serena Smith',
+        registrationDate: '7-19-2023',
+        model: 'Mustang',
+        make: 'Ford ',
+        location: 'Las Vegas, Nevada',
+        licensePlate: "245 - X64",
+        imageUrl: "/images/licensePlate7NV.jpg",
+        id: 7
+    }, 
+    {
+        owner: 'Halle Richardson',
+        registrationDate: '5-30-2023',
+        model: ' MX-5 Miata',
+        make: 'Mazda',
+        location: 'Philadelphia, Pennsylvania',
+        licensePlate: "ABC1234",
+        imageUrl: "/images/licensePlate8PA.jpg",
+        id: 8
     }, 
 ]
 
@@ -72,11 +105,19 @@ export default function Results(){
             <h1>Search Results</h1>
             <SearchBar onSearch={setSearchTerm}/> {/* Passing update searchterm prop to SearchBar */}
             
-            <p>Data will be displayed here.</p>
             {filteredResults.length > 0 ? ( // checkign if there are matching results
-                <PlateDetails results={filteredResults} /> // if true, passing results prop to plate details 
+                <div className="results-container">
+                    <PlateDetails results={filteredResults} /> {/* if true, passing results prop to plate details*/ }
+                    <div className="results-image-container">
+                        <PlateImage
+                            imageUrl={filteredResults[0].imageUrl} // if there are results, renders PlateDetails and PlateImage
+                            licensePlate={filteredResults[0].licensePlate}
+                            // passing back the first image in the filter. If the full owner name, make or model is typed. The correct image will be displayed
+                        />
+                    </div>
+                </div>
             ) : (
-                <p>No results found</p>
+                <p>No results found</p> // if there are no results, displays this message
             )}
         </div>
     );
