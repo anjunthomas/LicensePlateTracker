6efc33fb695bca10172a624c3a64d904bc8c9496
Victoria Valdes <vvaldes0520@gmail.com>
2025-03-09 16:33:03 -0400

    Changed webstie header

--- src/pages/FileUpload.jsx
+++ src/pages/FileUpload.jsx
@@ -2,7 +2,8 @@
 export default function FileUpload() {
     return (
       <div>
-        <h1>File Upload Page</h1>
+        <h1>License Plate Detector</h1>
+        <h2>File Upload</h2>
         <input type="file" /> {/*Let's the user choose one or more files from their device storage*/}
       </div>
     );
