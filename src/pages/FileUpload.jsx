9748aee804f6845a9719ae1d2bf73600cdc70daa
Victoria Valdes <vvaldes0520@gmail.com>
2025-03-09 16:39:22 -0400

    Added welcome sentence

--- src/pages/FileUpload.jsx
+++ src/pages/FileUpload.jsx
@@ -3,7 +3,9 @@ export default function FileUpload() {
     return (
       <div>
         <h1>License Plate Detector</h1>
-        <h2>File Upload</h2>
+        <p>Welcome to our license plate detector! Please drag or drop a
+          picture/video to begin.
+        </p>
         <input type="file" /> {/*Let's the user choose one or more files from their device storage*/}
       </div>
     );
