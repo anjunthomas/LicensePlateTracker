9fe01ca57d8fdf4999bbedece927eb4c75cd1249
Victoria Valdes <vvaldes0520@gmail.com>
2025-03-09 21:15:45 -0400

    Removed file upload input

--- src/pages/FileUpload.jsx
+++ src/pages/FileUpload.jsx
@@ -1,13 +1,16 @@
-import "../styles/DragDrop.css"
+import "../styles/DragDrop.css" // CSS styling
+import React from 'react';
+import { useDropzone } from 'react-dropzone';
 
 export default function FileUpload() {
+
     return (
       <div>
         <h1>License Plate Detector</h1>
         <p>Welcome to our license plate detector! Please drag or drop a
           picture/video to begin.
         </p>
-        <input type="file" /> {/*Let's the user choose one or more files from their device storage*/}
+        {/*Let's the user choose one or more files from their device storage*/}
       </div>
     );
 }
