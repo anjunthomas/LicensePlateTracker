a5c853308a1b2c1c3eac7d32f24c18833ec7fb0c
Victoria Valdes <vvaldes0520@gmail.com>
2025-03-10 00:08:55 -0400

    Finished tutorial for component on how to make drag and drop file upload

--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { useState } from 'react'
 import { useDropzone } from 'react-dropzone';
 import "../styles/DragDrop.css"
 
+// getRootProps
+    // Provides props that should be spread on the container element 
+    // (div) that acts as the drop zone.
+    // Handles drag and drop functionality
+    // Attaches drag-and-drop event listeners (onDragEnter, onDragLeave, onDrop)
+
+// getInputProps
+    // Provides props for the hidden <input> element that 
+    // allows users to select files by clicking.
+
 export default function DragDrop(){
-    const { getRootProps, getInputProps } = useDropzone();
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop: (acceptedFiles) => {
+            setUploadedFiles(acceptedFiles);
+        }
+    });
     return(
-        <div {...getRootProps} className = "file-upload">
+        <div {...getRootProps()} className = "file-upload">
             <input {...getInputProps()} />
+            <p>Drag and drop files here or click to browse.</p>
+            <ul>
+                {uploadedFiles.map((file) => (
+                    <li key={file.name}>{file.name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
