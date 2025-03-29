de5d3fdc64005e842e28077d12a999815a24fb22
Anju Thomas <anjunoblethomas@gmail.com>
2025-03-28 20:58:54 -0400

    Created database connection to MongoDB, updated index.js with connection

---
+++ backend/database/database.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE);
+        console.log("MongoDB successfully connected");
+    } catch(error){
+        console.error("MongoDB connection failed");
+    }
+};
+
+export default connectDB;
--- backend/server/index.js
+++ backend/server/index.js
@@ -1,10 +1,25 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from "express"; // setting up web server
+import dotenv from "dotenv"; // importing and configuring dotenv so it can read environment variables from a .env file like PORT=6000
 
-dotenv.config();
+dotenv.config(); // configuring dotenv
 
-const app = express();
+const app = express(); // creating an express app
 
-app.listen(process.env.PORT, () => {
-    console.log("App is running at port", process.env.PORT);
+console.log("PORT from env:", process.env.PORT);
+
+// path is /, which is the homepage of your backend
+// if someone visits http://localhost:6000/ function runs
+
+// setting up an HTTP GET route
+// req object = info from the client, headers, query, params
+// res = sends data back to the client
+app.get("/", (req, res) => {
+    res.send("Welcome to the license Plate Tracker API!");
+});
+
+import connectDB from "../database/database.js";
+connectDB();
+
+app.listen(process.env.PORT, () => { // starting a server on the port defined in .env
+    console.log("App is running at port", process.env.PORT); // logging the message
 });
